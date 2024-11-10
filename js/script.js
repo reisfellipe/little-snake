d32e22c06430e79273987554b7c0d2c57108a0a7
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-09 21:57:21 -0300

    saving progress

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// ctx.fillStyle = "green";
+// ctx.fillRect(300, 300, 50, 50);
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const size = 30;
+const snake= [
+    { x: 200, y: 200 },
+    { x: 230, y: 200 },
+    { x: 260, y: 200 },
+]
+
+const drawSnake = () => {
+    ctx.fillStyle = "lightgreen";
+    
+    snake.forEach(() => )
+}
+
+drawSnake();
